73caa2d11eff30e07bde38aa9cf30a5cac78aa86
nihalybaig <nihalybaig@ngenux.com>
2020-11-07 21:48:48 +0530

    404 page location.href

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const NotFoundPage = () => {
+const NotFoundPage = ({ location }) => {
   const [counter, setCounter] = useState(10)
   useEffect(() => {
     if (counter === 0) {
@@ -21,8 +21,8 @@ const NotFoundPage = () => {
       <div style={{ maxWidth: 750, margin: "5% auto" }}>
         <h1>404: Not Found</h1>
         <p>
-          <em>{window.location.href}</em> - This page doesn&#39;t exist... Page
-          under construction...
+          <em>{location.href}</em> - This page doesn&#39;t exist... Page under
+          construction...
         </p>
         <p>Redirecting to homepage in {counter} seconds</p>
         <Link to="/">Go to homepage</Link>
