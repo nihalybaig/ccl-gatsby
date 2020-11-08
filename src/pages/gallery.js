5b4d57ef9c363018753e82fb99803f3c8dbf2541
nihalybaig <nihalybaig@ngenux.com>
2020-11-08 23:06:55 +0530

    gallery start

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -4,15 +4,12 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import BreadcrumbComponent from "../components/Breadcrumb"
 
-const Gallery = () => (
+const GalleryPage = () => (
   <Layout>
     <SEO title="Gallery" />
     <BreadcrumbComponent current="Gallery" />
-    <div style={{ maxWidth: 960, margin: "0 auto" }}>
-      <h1>Hi from the second page</h1>
-      <p>Welcome to page 2</p>
-    </div>
+    <div style={{ maxWidth: 960, margin: "0 auto" }}>{`<Gallery />`}</div>
   </Layout>
 )
 
-export default Gallery
+export default GalleryPage
