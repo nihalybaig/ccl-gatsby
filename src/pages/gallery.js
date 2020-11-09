375f579cb06c71e36be9be28f70a3a8a13b2d80e
nihalybaig <nihalybaig@ngenux.com>
2020-11-09 22:23:25 +0530

    gallery image and node packages for gallery

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -3,12 +3,15 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import BreadcrumbComponent from "../components/Breadcrumb"
+import GalleryPhotos from "../components/GalleryPhotos"
 
 const GalleryPage = () => (
   <Layout>
     <SEO title="Gallery" />
     <BreadcrumbComponent current="Gallery" />
-    <div style={{ maxWidth: 960, margin: "0 auto" }}>{`<Gallery />`}</div>
+    <div style={{ maxWidth: 960, margin: "0 auto" }}>
+      <GalleryPhotos />
+    </div>
   </Layout>
 )
 
