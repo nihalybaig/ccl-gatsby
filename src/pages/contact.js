515e19348d1f1b8339e95c5f64a2adc8b0caa585
nihalybaig <nihalybaig@ngenux.com>
2020-12-29 21:53:50 +0530

    NnN: cclarabia feedback changes-contact map

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -19,11 +19,10 @@ const Contact = () => (
             <div className="col-lg-12 py-3">
               <iframe
                 title="contact map"
-                src="https://maps.app.goo.gl/5am1Zd3uhjukhXX76"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14289.097692307105!2d50.06727213122498!3d26.446881783285043!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e49fc3f3af64ecf%3A0xc43aec8081f68cb6!2sDammam%2032426%2C%20Saudi%20Arabia!5e0!3m2!1sen!2sin!4v1609258942142!5m2!1sen!2sin"
                 width="100%"
                 height="500px"
-                style={{ border: `0` }}
-                frameBorder="0"
+                frameborder="0"
                 allowFullScreen=""
                 aria-hidden="false"
               ></iframe>
